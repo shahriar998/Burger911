71994dab0f87bb990dd7c2d48bb17cf02e8a4fcb
Shahriar Elias <shahriar998@gmail.com>
2019-09-07 15:36:17 -0400

    finished db folder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Burger911
 Burger logger with MySQL, Node, Express, Handlebars and a homemade ORM
+
+# General Information 
+* Burger911! is a restaurant app that lets users input the names of burgers they'd like to eat.
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Egg Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
